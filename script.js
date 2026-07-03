03d57421772763f3ee5c51cc026bd94602bdce6b
waynlxn <waynelxn@duck.com>
2026-07-04 00:34:52 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let votes = 0;
-
-const voteCountElement = document.getElementById('voteCount');
-const agreeButton = document.getElementById('agreeButton');
-
-agreeButton.addEventListener('click', () => {
-    votes++;
-    voteCountElement.textContent = `Votes: ${votes}`;
-});
